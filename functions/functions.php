a6977af5ee47f89f5a194b503f3581b2b81c51f1
Jakub Kułaga <kuba.kulaga.sv7@gmail.com>
2020-08-02 13:47:18 +0100

    Add hook for displaying coachs in tribe events template

--- functions/functions.php
+++ functions/functions.php
@@ -21,3 +21,22 @@ function c_get_settings()
 {
 	return apply_filters('c_get_settings', get_option(C_TEXTDOMAIN . '-settings'));
 }
+
+/**
+ * Render coach
+ */
+add_action('studium_nvc_after_content_with_event', function ($event) {
+	$coachs = Timber\Timber::get_posts([
+		'post_type' => Coachs\Internals\PostTypes::POST_TYPE,
+		'post_status' => 'publish',
+		'numberposts' => -1,
+		'order'    => 'ASC',
+		'meta_query' => [
+			'key' => Coachs\Integrations\CMB::COACHS_TRAININGS_ID,
+			'value' => $event->id,
+			'compare' => 'LIKE'
+		]
+	]);
+
+	Timber\Timber::render('Coachs_Widget/coachs-loop.html.twig', ['coachs' => $coachs]);
+}, 10, 1);
